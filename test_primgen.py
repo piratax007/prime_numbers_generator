55303e237dfeccd6beb66eeb2e5ec31a570ec2db
Fausto Mauricio <fausto.lagos@colseminario.edu.co>
2021-09-03 21:57:07 -0500

    Add tests

---
+++ test_primgen.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_random_odd_integer(self):
+        from primgen import random_odd_integer
+        self.assertTrue(random_odd_integer(64) % 2 == 1)
+
+    def test_miller_rabin_method(self):
+        from primgen import miller_rabin_method
+        self.assertTrue(miller_rabin_method(632485520306600051477731141302573178632491, .9) is True)
+        self.assertTrue(miller_rabin_method(989086760284979193666271685572629563840964, .99) is False)
